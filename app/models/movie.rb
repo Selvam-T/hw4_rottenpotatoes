5b2be429fef7926f3848f800dbeccddd31011212
saasbook <saasbook@saasbook.(none)>
2012-08-20 04:57:28 -0700

    director field added to the model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,13 +2,4 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
-  
-  def up
-    add_column :movies, :director, :string, :default => nill
-  end
-
-  def down
-    remove_column :movies, :director
-  end
-  
 end
